deddc4adf03aa2a2f2081561dbee29805c94f4b8
Madhab Tripathy <madhabtripathy1349@gmail.com>
2023-08-19 18:42:18 +0530

    second

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/Profile" element={<Profile />} /> 
+        <Route path="/react-login-ui-phi.vercel.app/Profile" element={<Profile />} /> 
         
     </Routes>
   );
